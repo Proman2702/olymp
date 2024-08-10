1e226fade6be25bcd71c49af35946ae9221cebac
Proman2702 <proman2702@gmail.com>
2024-08-11 00:00:50 +0300

    test3

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'src/wigdets/list_widget.dart';
+import 'src/wigdets/add_audio.dart';
 
 void main() => runApp(App());
 
@@ -23,17 +24,29 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            "Test",
-            style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w800),
+        elevation: 3,
+        toolbarHeight: 70,
+        shadowColor: Colors.deepPurple,
+        leadingWidth: 100, titleSpacing: 10,
+        // shape: RoundedRectangleBorder(
+        //   borderRadius: BorderRadius.only(
+        //     bottomLeft: Radius.elliptical(200, 20), bottomRight: Radius.elliptical(200, 20)
+        //     )
+        // ),
+        title: Container(
+          padding: EdgeInsets.all(5),
+            child: Text(
+              "Test",
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w800, color: Colors.white),
+            ),
           ),
-        ),
-        backgroundColor: Colors.amber,
+        backgroundColor: Color.fromARGB(255, 161, 29, 201),
       ),
       body: Center(
         child: const MainWidget(),
+      
       ),
+      floatingActionButton: AddButton(),
     );
   }
 }
