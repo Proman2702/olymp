7dbb8c25abfe00c066ac3702fbf2541082d9ed96
Proman2702 <proman2702@gmail.com>
2024-08-10 20:16:50 +0300

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'src/wigdets/list_widget.dart';
 
 void main() => runApp(App());
 
@@ -33,40 +34,6 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: const MainWidget(),
       ),
-    );
-  }
-}
-
-class MainWidget extends StatelessWidget {
-  const MainWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          alignment: Alignment.center,
-          color: Colors.orange,
-          width: 250,
-          height: 50,
-          child: Text(
-            "привет",
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
-          ),
-        ),
-        SizedBox(
-          height: 30,
-        ),
-        Container(
-          alignment: Alignment.center,
-          color: Colors.orange,
-          width: 100,
-          height: 20,
-          child: Text("бурда"),
-        )
-      ],
     );
   }
 }
