a3fde3b6bd4299c39fb2eb908d1676db1e5fd26b
Proman2702 <proman2702@gmail.com>
2024-08-20 21:56:27 +0300

    start working

--- lib/src/wigdets/list_widget.dart
+++ lib/src/wigdets/list_widget.dart
@@ -1,6 +1,4 @@
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/src/audio_player.dart';
 
 class MainWidget extends StatefulWidget {
   const MainWidget({super.key});
@@ -10,31 +8,6 @@ class MainWidget extends StatefulWidget {
 }
 
 class _MainWidgetState extends State<MainWidget> {
-  List<Widget> list_of_players = [];
-  int counter = 0;
-  void _add_audio_to_list(PlatformFile file) {
-    setState(() {
-      counter++;
-      list_of_players.add(const SizedBox(
-        height: 20,
-      ));
-      list_of_players.add(Container(
-        height: 80,
-        width: 350,
-        decoration: const ShapeDecoration(
-            color: Color.fromARGB(255, 217, 191, 229),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20)))),
-        alignment: Alignment.center,
-        child: Row(children: [
-          Text("Аудио $counter"),
-          ElevatedButton(
-              onPressed: Player(file: file).start, child: const Text("play"))
-        ]),
-      ));
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,18 +35,11 @@ class _MainWidgetState extends State<MainWidget> {
       ),
       body: Center(
           child: Container(
-        child: Column(children: list_of_players),
+        child: Column(children: <Widget>[Text("бурда")]),
       )),
       floatingActionButton: FloatingActionButton(
         backgroundColor: const Color.fromARGB(255, 161, 29, 201),
-        onPressed: () async {
-          var picked = await FilePicker.platform.pickFiles();
-          if (picked == null) return;
-
-          var file = picked.files.first;
-
-          _add_audio_to_list(file);
-        },
+        onPressed: () {},
         child: const Center(
             child: Text(
           "+",
