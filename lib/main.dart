3292c96366382b3aea4d6884d8e85a1ce55495e6
Proman2702 <proman2702@gmail.com>
2024-08-25 19:19:17 +0300

    working2.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'src/wigdets/main_widget.dart';
+import 'package:my_app/src/colors/appbar_grad.dart';
 
 void main() => runApp(App());
 
@@ -10,10 +10,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'test',
-        theme: ThemeData(fontFamily: 'Nunito'),
-        home: HomePage());
+    return MaterialApp(routes: {
+      "/": (context) => HomePage(),
+      "/settings": (context) => SettingsPage()
+    }, title: 'test', theme: ThemeData(fontFamily: 'Nunito'));
   }
 }
 
@@ -22,6 +22,88 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainWidget();
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 4,
+          toolbarHeight: 70,
+          shadowColor: Colors.deepPurple,
+          leadingWidth: 20,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(gradient: MainGrad()),
+          ),
+          title: Container(
+            padding: const EdgeInsets.all(5),
+            child: const Text(
+              "AI App",
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white),
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed("/settings");
+              },
+              icon: Icon(Icons.settings),
+              color: Colors.white,
+              iconSize: 35,
+            ),
+            Container(width: 10)
+          ]),
+      body: Center(
+          child: Container(
+        child: Column(children: <Widget>[Text("анти-бурда")]),
+      )),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromARGB(255, 161, 29, 201),
+        onPressed: () {},
+        child: const Center(
+            child: Text(
+          "+",
+          textScaler: TextScaler.linear(2.4),
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w800),
+        )),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 4,
+          toolbarHeight: 70,
+          shadowColor: Colors.deepPurple,
+          leadingWidth: 50,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(gradient: MainGrad()),
+          ),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.arrow_back),
+            iconSize: 35,
+            color: Colors.white,
+          ),
+          title: Center(
+            child: Container(
+              padding: const EdgeInsets.only(right: 30),
+              child: const Text(
+                "Настройки",
+                style: TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+            ),
+          )),
+    );
   }
 }
